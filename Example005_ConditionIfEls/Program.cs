06a47fe77236395dc690e08863cf851edd261491
Andrei Jermakov <jermakov.andrei@gmail.com>
2021-12-20 13:26:06 +0200

    Example 005 Condition If Else

---
+++ Example005_ConditionIfEls/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vvedite imja Polzovatelja ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, this is Masha!");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
